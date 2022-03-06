01516f65a1362e58e25bcf135b61cb8442eacfb9
Zoha Akram <zoha.ak24@gmail.com>
2022-03-07 00:22:54 +0500

    update user

--- contracts/supplychain.sol
+++ contracts/supplychain.sol
@@ -5,9 +5,9 @@ contract SupplyChain{
     address public owner;
     uint public creationTime;
     bytes32 a;
-mapping(address => User) private usersdetail;
-address[] private userindex;
-uint count =userindex.length;
+    mapping(address => User) private usersdetail;
+    address[] private userindex; // array of address of all users
+    uint count =userindex.length;
 
     constructor()  {
         owner=msg.sender;
@@ -19,15 +19,15 @@ uint count =userindex.length;
             return (userindex[usersdetail[_userAddress].index] == _userAddress);
       }
          
-    struct us_roles{
-       uint us_role;
+    struct user_roles{
+       uint user_role;
     }
-mapping(address=>uint) us_role;
+mapping(address=>uint) user_role;
 
         struct User{
             uint role;
             string password;
-            string us_name;
+            string userName;
             address createdBy;
             uint creationTime;
             string location;
@@ -35,34 +35,50 @@ mapping(address=>uint) us_role;
             string email;
             
 
-        }
+        } event LogUpdateUser(address indexed _userAddress, uint index,  string email, string location, string password);
            event LogNewUser   (address indexed _userAddress, uint index,string  name, string password,  string email, uint userrole, bytes32 _hash);
-        function setUser(address _userAddress, string memory us_name, string memory password, string memory email,string memory location, uint role) public returns(bytes32 _a,uint index) {
+        function setUser(address _userAddress, string memory userName, string memory password, string memory email,string memory location, uint role) public returns(bytes32 _a,uint index) {
 require(isUser(_userAddress)==false);
  usersdetail[_userAddress].createdBy = msg.sender;
             usersdetail[_userAddress].creationTime= block.timestamp;
-            usersdetail[_userAddress].us_name=us_name;
+            usersdetail[_userAddress].userName=userName;
             usersdetail[_userAddress].password=password;
             usersdetail[_userAddress].email=email;
             usersdetail[_userAddress].location=location;
             usersdetail[_userAddress].role=role;
-            us_role[_userAddress]= role;
+            user_role[_userAddress]= role;
             userindex.push(_userAddress);
             usersdetail[_userAddress].index=userindex.length-1;
-           a=sha256(abi.encodePacked(_userAddress,us_name,location,password,email,role));
+           a=sha256(abi.encodePacked(_userAddress,userName,location,password,email,role));
         
              emit  LogNewUser(
             _userAddress,
             usersdetail[_userAddress].index,
-            us_name,
+            userName,
             password,
             email,
-            us_role[_userAddress],
+            user_role[_userAddress],
             a
             );
            
              return (a,userindex.length-1);
         
 
-        }}
+        }
+        function updateUser(address _userAddress, string memory email, string memory location, string memory password) public returns(bool success) {
+           
+            require(isUser(_userAddress)==true) ;
+            usersdetail[_userAddress].email = email;
+            usersdetail[_userAddress].location = location;
+            usersdetail[_userAddress].password = password;
+
+            emit LogUpdateUser(
+            _userAddress,
+            usersdetail[_userAddress].index,
+            email,
+            location,
+            password
+            );
+            return true;
+        }  }
 
