c1e182076269299a5308a65494b0ea3f677c81a9
Zoha Akram <zoha.ak24@gmail.com>
2022-03-13 19:00:49 +0500

    shipped by supplier added

--- contracts/supplychain1.sol
+++ contracts/supplychain1.sol
@@ -2,15 +2,16 @@
 pragma solidity >=0.4.20;
 
 contract SupplyChain {
-  mapping(address => address[]) item_map;
+
+
+  mapping(address=>Item)
   mapping(bytes32 => Item) itemIdentity;
+
 
   address contractOwner;
   bytes32 upc;
 
-  constructor() {
-    contractOwner = msg.sender;
-  }
+
 
   enum State {
     producedBySupplier,
@@ -25,7 +26,7 @@ contract SupplyChain {
     shippedByManufacturer,
     receivedByRetailer
   }
-  State constant defaultState = State.producedBySupplier;
+  State constant defaultState = State.ProducedBySupplier;
 
   struct Item {
     bytes32 upc;
@@ -42,26 +43,13 @@ contract SupplyChain {
   //modifier
   modifier onlyOwner() {
     require(msg.sender == contractOwner, 'only ownercan do this');
-    _;
   }
-  modifier verifyCaller(address _userAddress) {
-    require(msg.sender == _userAddress);
-    _;
-  }
+
   //events
   event producedBySupplier(bytes32 upc);
   event forSaleBySupplier(bytes32 upc, uint256 price);
-  event lognewPurchase(
-    bytes32 upc,
-    uint256 time,
-    uint256 qty,
-    address supplierID
-  );
-  event purchasedByManufacturer(bytes32 upc);
-  event shippedBySupplier(bytes32 _upc);
-
+  event lognewPurchase(bytes upc, uint time, uint qty, address supplierID)
   function itemBySupplier(
-    // not confirm about flavor and qty to form upc hash
     uint256 weight,
     uint256 flavor,
     uint256 _productType,
@@ -71,7 +59,7 @@ contract SupplyChain {
     itemIdentity[upc] = Item({
       upc: sha256(abi.encodePacked(weight, flavor, _productType, qty)),
       ownerID: contractOwner,
-      SupplierID: msg.sender,
+      supplierID: msg.sender,
       SupplierName: _SupplierName,
       productPrice: uint256(0),
       itemState: defaultState,
@@ -79,11 +67,10 @@ contract SupplyChain {
       retailerID: address(0),
       productType: _productType
     });
-
     emit producedBySupplier(upc);
   }
 
-  function forSalebySupplier(bytes32 _upc, uint256 _price) public {
+  function forSaleBySupplier(bytes32 _upc, uint256 _price) public {
     itemIdentity[_upc].SupplierID = msg.sender;
     itemIdentity[_upc].itemState = State.forSaleBySupplier;
     itemIdentity[_upc].productPrice = _price;
@@ -91,35 +78,15 @@ contract SupplyChain {
     emit forSaleBySupplier(_upc, _price);
   }
 
-  function orderByManufacturer(string memory desc)
-    public
-    view
-    returns (address orderBy, string memory description)
-  {
-    return (msg.sender, desc);
-  }
+  function purchaseItemByManufacturer(bytes32 _upc, uint256 qty) {
 
-  function purchaseItemByManufacturer(bytes32 _upc, uint256 qty) public {
-    itemIdentity[_upc].ownerID = msg.sender;
-    itemIdentity[_upc].manufacturerID = msg.sender;
-    itemIdentity[_upc].itemState = State.purchasedByManufacturer;
-    item_map[msg.sender].push(itemIdentity[_upc].SupplierID);
+      itemIdentity[_upc].ownerID=msg.sender;
+      itemIdentity[_upc].manufacturerID=msg.sender;
+      itemIdentity[_upc].itemState=State.purchasedByManufacturer;
+      item_map[msg.sender].push(itemIdentity[_upc].SupplierID);
 
-    emit lognewPurchase(
-      _upc,
-      block.timestamp,
-      qty,
-      itemIdentity[_upc].SupplierID
-    );
+      emit lognewPurchase(_upc, block.timestamp,qty,itemIdentity[_upc],supplierID);
 
-    emit purchasedByManufacturer(_upc);
-  }
-
-  function shippedItemBySupplier(bytes32 _upc)
-    public
-    verifyCaller(itemIdentity[_upc].SupplierID) // check msg.sender is supplierID
-  {
-    itemIdentity[_upc].itemState = State.shippedBySupplier; // update state
-    emit shippedBySupplier(_upc);
+      emit purchasedByManufacturer(_upc);
   }
 }
